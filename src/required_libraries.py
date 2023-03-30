f7940bbd50a75c89269145178af5559d90ecc007
dfarone02 <97635420+dfarone02@users.noreply.github.com>
2023-03-30 12:49:19 -0500

    req llibraries

---
+++ src/required_libraries.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+
+
+import datetime as dt
+from datetime import timedelta
+from datetime import datetime
+
+from sklearn import datasets, linear_model
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+lc = LabelEncoder()
+
+import statsmodels
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+from sklearn.model_selection import train_test_split, cross_val_score, RepeatedKFold, GridSearchCV
+from sklearn.metrics import mean_squared_error,r2_score,mean_absolute_error,mean_absolute_percentage_error
+
+from sklearn.pipeline import make_pipeline, Pipeline
+
+from numpy import arange
+from sklearn.linear_model import Lasso, Ridge
+from sklearn.linear_model import LogisticRegression
+from sklearn.kernel_ridge import KernelRidge
+
+
+from sklearn import tree
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier, export_graphviz
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, BaggingRegressor, RandomForestRegressor
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import plot_confusion_matrix, accuracy_score
+from sklearn.metrics import roc_curve, auc, plot_roc_curve, roc_auc_score
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.metrics import plot_roc_curve
+
+from tqdm import tqdm
+import ydata_profiling
+
+#import precision and recall
+from sklearn.metrics import precision_score, recall_score
+
+# Trick to widen the screen
+from IPython.core.display import display, HTML
+
+#Widens the code landscape 
+display(HTML("<style>.container { width:95% !important; }</style>"))
+
+import warnings
+warnings.filterwarnings('ignore')
+
+pd.set_option('display.max_columns',500)
+pd.set_option('display.max_rows', 50)
+sns.set_style('whitegrid')
+
